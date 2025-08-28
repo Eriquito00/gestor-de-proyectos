6b99bc5c70ba274bd3a84f97928b12d289242866
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-08-29 01:42:27 +0200

    database and database diagrams

---
+++ backend/bbdd/files/ddl.sql
@@ -0,0 +1,39 @@
+DROP TABLE tasks;
+DROP TABLE lists;
+DROP TABLE projects;
+
+CREATE TABLE projects (
+	project_id		INTEGER,
+	title			VARCHAR(50) NOT NULL UNIQUE,
+	description		VARCHAR(150),
+	CONSTRAINT pk_projects PRIMARY KEY (project_id AUTOINCREMENT)
+);
+
+CREATE TABLE lists (
+	list_id			INTEGER,
+	title			VARCHAR(15) UNIQUE,
+	project_id		INT NOT NULL,
+	CONSTRAINT pk_lists PRIMARY KEY (list_id AUTOINCREMENT),
+	CONSTRAINT fk_lists_projects FOREIGN KEY (project_id)
+		REFERENCES projects (project_id)
+);
+
+CREATE TABLE tasks (
+	task_id			INTEGER,
+	title			VARCHAR(15) NOT NULL UNIQUE,
+	description		VARCHAR(150),
+	list_id			INT UNSIGNED,
+	CONSTRAINT pk_tasks PRIMARY KEY (task_id AUTOINCREMENT),
+	CONSTRAINT fk_tasks_lists FOREIGN KEY (list_id)
+		REFERENCES lists (list_id)
+);
+
+
+INSERT INTO projects(title, description)
+	VALUES("Proyecto de prueba", "Hola soy una descripcion de prueba");
+	
+INSERT INTO lists(title, project_id)
+	VALUES("Lista de prueba", 1);
+	
+INSERT INTO tasks(title, description, list_id)
+	VALUES("Tarea de prueba", "Hola soy una description de prueba", 1);
