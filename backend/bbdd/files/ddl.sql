a5284f8e871fdfadc7689b6b21b7470e6a579825
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2025-09-02 18:09:38 +0200

    bbdd updates

--- backend/bbdd/files/ddl.sql
+++ backend/bbdd/files/ddl.sql
@@ -3,37 +3,37 @@ DROP TABLE lists;
 DROP TABLE projects;
 
 CREATE TABLE projects (
-	project_id		INTEGER,
+	project_id		CHAR(36),
 	title			VARCHAR(25) NOT NULL UNIQUE,
 	description		VARCHAR(150),
-	CONSTRAINT pk_projects PRIMARY KEY (project_id AUTOINCREMENT)
+	CONSTRAINT pk_projects PRIMARY KEY (project_id)
 );
 
 CREATE TABLE lists (
-	list_id			INTEGER,
+	list_id			CHAR(36),
 	title			VARCHAR(15) UNIQUE,
-	project_id		INT NOT NULL,
-	CONSTRAINT pk_lists PRIMARY KEY (list_id AUTOINCREMENT),
+	project_id		CHAR(36),
+	CONSTRAINT pk_lists PRIMARY KEY (list_id),
 	CONSTRAINT fk_lists_projects FOREIGN KEY (project_id)
 		REFERENCES projects (project_id)
 );
 
 CREATE TABLE tasks (
-	task_id			INTEGER,
+	task_id			CHAR(36),
 	title			VARCHAR(15) NOT NULL UNIQUE,
 	description		VARCHAR(150),
-	list_id			INT UNSIGNED,
-	CONSTRAINT pk_tasks PRIMARY KEY (task_id AUTOINCREMENT),
+	list_id			CHAR(36),
+	CONSTRAINT pk_tasks PRIMARY KEY (task_id),
 	CONSTRAINT fk_tasks_lists FOREIGN KEY (list_id)
 		REFERENCES lists (list_id)
 );
 
 
-INSERT INTO projects(title, description)
-	VALUES("Proyecto de prueba", "Hola soy una descripcion de prueba");
+INSERT INTO projects(project_id, title, description)
+	VALUES("f47ac10b-58cc-4372-a567-0e02b2c3d479", "Proyecto de prueba", "Hola soy una descripcion de prueba");
 	
-INSERT INTO lists(title, project_id)
-	VALUES("Lista de prueba", 1);
+INSERT INTO lists(list_id, title, project_id)
+	VALUES("f36ac09b-47cc-3261-a456-9e91b1c2d368", "Lista de prueba", "f47ac10b-58cc-4372-a567-0e02b2c3d479");
 	
-INSERT INTO tasks(title, description, list_id)
-	VALUES("Tarea de prueba", "Hola soy una description de prueba", 1);
+INSERT INTO tasks(task_id, title, description, list_id)
+	VALUES("f25ac98b-36cc-2150-a345-8e80b0c1d257", "Tarea de prueba", "Hola soy una description de prueba", "f36ac09b-47cc-3261-a456-9e91b1c2d368");
